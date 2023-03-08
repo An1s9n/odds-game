d877949f5897d0fcc9f2e15e697df6f4a4311359
An1s9n <An1s9n@yandex.ru>
2023-03-08 06:50:38 +0300

    Add OpenApiDefinition, remove SNAPSHOT version

--- src/main/kotlin/ru/an1s9n/odds/game/config/AppConfig.kt
+++ src/main/kotlin/ru/an1s9n/odds/game/config/AppConfig.kt
@@ -1,12 +1,21 @@
 package ru.an1s9n.odds.game.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationPropertiesScan
+@OpenAPIDefinition(
+  info = Info(
+    title = "\${app.group-id}:\${app.artifact-id}",
+    version = "\${app.version}",
+    description = "REST API for simple odds-based game. For more information see sources at https://github.com/An1s9n/odds-game"
+  )
+)
 @SecurityScheme(
   type = SecuritySchemeType.HTTP,
   scheme = "Bearer",
